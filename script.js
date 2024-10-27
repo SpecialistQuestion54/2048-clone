6158809df8602966269a9d2830e558a322e81674
SpecialistQuestion54 <kailkhuradivyansh@gmail.com>
2024-10-27 17:35:23 +0530

    implemented down movement as per game rules

--- script.js
+++ script.js
@@ -3,31 +3,59 @@ const score_text = document.querySelector(".score-value");
 new_game_btn.addEventListener('click', function () {
     location.reload();
 });
-let board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+let board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
 let grid = document.querySelector(".grid");
 let score = 0;
 let selected_tile = null;
 console.log(board);
-function moveWholeBoardDown(){
-    for(let i=0; i<4; i++){
-        
+
+function updateDisplay() {
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            const target_tile_id = (i * 4 + j).toString();
+            const target_tile = document.getElementById(target_tile_id);
+            const tti_className = board[i][j] == 0 ? "cell" : "tile-" + board[i][j].toString();
+            target_tile.className = tti_className.indexOf("-") == -1 ? "cell" : tti_className;
+            target_tile.innerText = tti_className.indexOf("-") == -1 ? "" : tti_className.split("-")[1];
+        }
     }
+    console.log(board);
+}
+function moveWholeBoardDown() {
+    let f = false;
+    for (let j = 0; j < 4; j++) {
+        let col = board.map(x => x[j]);
+        col = col.filter(x => x != 0);
+        for (let i = col.length - 1; i > 0; i--) { 
+            if (col[i] === col[i - 1]) {
+                col[i] *= 2;
+                col[i - 1] = 0;  
+                f = true;
+            }
+        }
+        col = col.filter(value => value != 0);
+        let k = 4 - col.length;
+        while (k--) {
+            f = true;
+            col.unshift(0);
+        }
+        for (i = 0; i < 4; i++)
+            board[i][j] = col[i];
+
+    }
+
+    if (f) random_Spawn();
+    updateDisplay();
 }
 function random_Spawn() {
     let sp = Math.floor(Math.random() * 16);
-    if (board[Math.floor(sp/4)][Math.floor(sp%4)] == 0) {
+    if (board[Math.floor(sp / 4)][Math.floor(sp % 4)] == 0) {
         const start = document.getElementById(sp.toString());
         start.className = "tile-2";
         start.innerText = start.className.split("-")[1];
-        board[Math.floor(sp/4)][sp%4] = 2;
-    }else random_Spawn();
+        board[Math.floor(sp / 4)][sp % 4] = 2;
+    } else random_Spawn();
 
-}
-function deterministic_spawn(tile_type, id) {
-    const start = document.getElementById(id);
-    start.className = "tile-" + tile_type;
-    start.innerText = start.className.split("-")[1];
-    board[id] = Number(tile_type);
 }
 random_Spawn();
 random_Spawn();
@@ -35,60 +63,22 @@ random_Spawn();
 
 grid.addEventListener("click", function (event) {
     const target = event.target;
-    if (target.className.startsWith("tile-")) {
-        // Tile is selected
-        if (selected_tile) {
-            selected_tile.style.border = "none";
-
-            const tid = Number(target.id);
-            const sid = Number(selected_tile.id);
-
-            if ((tid % 4 == sid % 4 || Math.floor(tid / 4) == Math.floor(sid / 4)) && target.className === selected_tile.className && target.id != selected_tile.id) {
-                selected_tile.className = "cell";
-                selected_tile.innerText = "";
-                selected_tile.style.border = "none"; // Remove selection border
-                board[Math.floor(sid/4)][sid%4] = 0;
-                selected_tile = null; 
-
-                const type = (Number(target.className.split("-")[1]) * 2).toString();
-                target.className = "tile-" + type;
-                target.innerText = target.className.split("-")[1];
-
-                board[Math.floor(tid / 4)][tid%4] = Number(target.className.split("-")[1]);
-                target.style.border = "none";
-                score += Number(type);
-                score_text.innerText = score;
-                random_Spawn();
-                console.log(board);
-            }
-
-        } // Remove border from previously selected tile
+    if (selected_tile) {
+        selected_tile.style.border = "none";
+        const s = Number(selected_tile.id);
+        const t = Number(target.id);
+        if (Math.floor(s / 4) < Math.floor(t / 4)) {
+            moveWholeBoardDown();
+        }
+        selected_tile = null;
+    } else {
         selected_tile = target;
-        selected_tile.style.border = "2px solid black"; // Add border to current selection
-    }
-    else if (target.className === "cell" && selected_tile) {
-        // Move the selected tile to the empty cell
-        const tid = Number(target.id);
-        const sid = Number(selected_tile.id);
-        if (tid % 4 == sid % 4 || Math.floor(tid / 4) == Math.floor(sid / 4)) {
-            target.className = selected_tile.className;
-            target.innerText = selected_tile.innerText;
-
-            // Reset the original tile back to a cell
-            selected_tile.className = "cell";
-            selected_tile.innerText = "";
-            selected_tile.style.border = "none"; // Remove selection border
-            board[Math.floor(sid/4)][sid%4] = 0;
-            board[Math.floor(tid / 4)][tid%4] = Number(selected_tile.id);
-            selected_tile = null; // Clear the selection
-            random_Spawn();
-            console.log(board);
-        } else selected_tile.style.border = "none";
+        selected_tile.style.border = "3px solid black";
     }
 });
 
 grid.addEventListener('contextmenu', function (event) {
     event.preventDefault();
-    selected_tile.style.border = "none";
+    if (selected_tile) selected_tile.style.border = "none";
     selected_tile = null;
 })
